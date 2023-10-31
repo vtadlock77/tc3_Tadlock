71b71986b521fe6dec915fb0f622b235d8762290
vtadlock77 <145047306+vtadlock77@users.noreply.github.com>
2023-10-31 15:37:25 -0500

    create output

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0","p3"]},{w:"Kalura:",p:["p1"]},{w:"Closed",p:["p1"]},{w:"Captioning",p:["p1"]},{w:"Process",p:["p1"]},{w:"Overview",p:["p1","p5"]},{w:"Cost",p:["p2"]},{w:"Get",p:["p3"]},{w:"Intended",p:["p4"]},{w:"Audience",p:["p4"]},{w:"Kaltura",p:["p5","p6","p7","p9","p12","p13"]},{w:"Accessing",p:["p6"]},{w:"Canvas:",p:["p7","p11"]},{w:"Create",p:["p7"]},{w:"a",p:["p7","p12","p13"]},{w:"Video",p:["p7","p12"]},{w:"Assignment",p:["p7","p12","p13"]},{w:"Highlights",p:["p8"]},{w:"The",p:["p9","p11"]},{w:"Do\'s",p:["p9"]},{w:"and",p:["p9"]},{w:"Do",p:["p9"]},{w:"Not\'s",p:["p9"]},{w:"of",p:["p9"]},{w:"Using",p:["p9","p12","p13"]},{w:"Resources",p:["p10"]},{w:"Add,",p:["p11"]},{w:"Move,",p:["p11"]},{w:"or",p:["p11"]},{w:"Remove",p:["p11"]},{w:"Media",p:["p11"]},{w:"Gallery",p:["p11"]},{w:"Setting",p:["p12"]},{w:"Up",p:["p12"]},{w:"Canvas",p:["p13"]},{w:"for",p:["p13"]},{w:"Students:",p:["p13"]},{w:"Submit",p:["p13"]},{w:"Video/Audio",p:["p13"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8];
+ph["p3"]=[9, 2];
+ph["p4"]=[10, 11];
+ph["p5"]=[12, 7];
+ph["p6"]=[13, 12];
+ph["p7"]=[14, 15, 16, 12, 17, 18];
+ph["p8"]=[19];
+ph["p9"]=[20, 21, 22, 23, 24, 25, 26, 12];
+ph["p10"]=[27];
+ph["p12"]=[34, 35, 16, 17, 18, 26, 12];
+ph["p11"]=[14, 28, 29, 30, 31, 20, 32, 33];
+ph["p13"]=[36, 37, 38, 39, 16, 40, 18, 26, 12];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {13:-1,7:-1,12:7,10:6,8:6,5:-1,2:5,1:-1,6:-1,9:6,4:5,3:5,11:-1};
