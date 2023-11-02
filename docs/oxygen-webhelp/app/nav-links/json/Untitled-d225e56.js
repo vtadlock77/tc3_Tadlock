410fe9e2f97f3d76d8ffd6c8688768cfe874d8ec
vtadlock77 <145047306+vtadlock77@users.noreply.github.com>
2023-11-02 15:38:31 -0500

    final product

---
+++ docs/oxygen-webhelp/app/nav-links/json/Untitled-d225e56.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"Untitled-d225e56":{"topics":[{"title":"Where to Go for Additional Reading?","shortdesc":"<p class=\"shortdesc\">This pages contains additional resources for using Kaltura.<\/p>","href":"topics\/Reference_Resources.html","attributes":{"data-id":"Reference_Resources"},"menu":{"hasChildren":false},"tocID":"Reference_Resources-d225e67","topics":[]}]}};
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {8:-1,7:8,1:-1,6:-1,3:5,4:5,9:8,10:6,2:5,11:-1,5:-1,14:-1};
+var linkToParent = {1:-1,10:6,5:-1,8:-1,9:8,2:5,14:-1,11:-1,7:8,6:-1,4:5,3:5};
