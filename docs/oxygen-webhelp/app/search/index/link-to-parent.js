95a363d6dbb7c40ed0565e3ee7ef0e488f3c2d45
vtadlock77 <145047306+vtadlock77@users.noreply.github.com>
2023-11-02 15:59:01 -0500

    final, final output

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {1:-1,10:6,5:-1,8:-1,9:8,2:5,14:-1,11:-1,7:8,6:-1,4:5,3:5};
+var linkToParent = {8:-1,7:8,14:-1,9:8,10:6,12:-1,5:-1,2:5,1:-1,6:-1,4:5,3:5,11:-1};
